f2eef5600e5b071cba99a887ac2dd86a020a457f
Dung Do <dzung.do@fruitful.io>
2020-04-02 00:40:44 -0400

    Work

---
+++ train_test.py
@@ -0,0 +1,138 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from keras import optimizers
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Dense, LSTM
+from keras.models import Sequential, model_from_json
+import sklearn
+from sklearn import preprocessing
+from sklearn.metrics import roc_auc_score, roc_curve, auc, confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+
+from preprocess import process_data
+
+X_Data, Y_Data = process_data(1, 5)
+
+enc = preprocessing.OneHotEncoder(categories='auto')
+enc.fit(Y_Data)
+Y_Data = enc.transform(Y_Data).toarray()
+
+X_train, X_test, Y_train, Y_test = train_test_split(X_Data, Y_Data, test_size=0.1)
+
+
+def create_lstm_model():
+    model = Sequential()
+
+    # input_data shape = (num_trials, timesteps, input_dim)
+    ## activation function:  tanh , relu,...
+    model.add(LSTM(500, input_shape=(5, 1685), return_sequences=True, activation='relu', dropout=0.7))
+    # returns a sequence of vectors of dimension 60
+    model.add(LSTM(400, activation='tanh', dropout=0.5, return_sequences=True))
+    # returns a sequence of vectors of dimension 60
+    model.add(LSTM(300, activation='tanh', dropout=0.6))
+    # return a single vector of dimension 40
+    model.add(Dense(5, activation="softmax"))  # number of classes 3, object categori,....
+
+    adam = optimizers.adam(lr=0.001, beta_1=0.9, beta_2=0.999)
+    model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+    return model
+
+
+def train(x_train, y_train):
+    # training:
+    # out_data shape = (num_trials, num_classes)
+    batch_size = 50  # 10,20,....
+    epochs = 50  # 20,30,...
+    model = create_lstm_model()
+
+    ## monitor accuracy and save the best models; it seems not much improve after 33 epochs
+    filepath = "./weights-improvement-{epoch:02d}-{val_accuracy:.2f}.hdf5"
+    checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    callbacks_list = [checkpoint]
+
+    model.fit(X_train, Y_train, validation_split=0.1, epochs=epochs, batch_size=batch_size, callbacks=callbacks_list,
+              verbose=0)
+
+    return model
+
+
+def evaluate(model):
+    Y_predict = model.predict(X_test)
+
+    scores = model.evaluate(X_test, Y_test, verbose=0)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+    # accuracy = sklearn.metrics.accuracy_score(Y_test, Y_predict)
+    # print("%s: %.2f%%" % (model.metrics_names[1], accuracy * 100))
+
+    roc_auc_score(Y_test, Y_predict)
+
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(5):
+        fpr[i], tpr[i], _ = roc_curve(Y_test[:, i], Y_predict[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(Y_test.ravel(), Y_predict.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    plt.figure()
+    lw = 2
+    plt.plot(fpr[2], tpr[2], color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+
+    M = model.predict_classes(X_test)
+
+    ytt = np.zeros((205, 1))
+    for i, val in enumerate(Y_test):
+        val = val.tolist()
+        ytt[i] = int(val.index(1.0))
+
+    report = classification_report(ytt, M)
+    print(report)
+
+    matrix = confusion_matrix(ytt, M)
+    print(matrix)
+
+    plt.imshow(matrix, interpolation=None, cmap='binary')
+    plt.title('confusion matrix')
+    plt.colorbar()
+    plt.title('confusion_matrix')
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+
+def save_model(model, model_file):
+    model_json = model.to_json()
+    with open(model_file, "w") as json_file:
+        json_file.write(model_json)
+    model.save_weights("model.h5")
+
+
+def load_model(model_file):
+    json_file = open(model_file, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model.h5")
+
+    adam = optimizers.adam(lr=0.001, beta_1=0.9, beta_2=0.999)
+    loaded_model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+
+    return loaded_model
+
+
+# lstm_model = train(X_train, Y_train)
+# save_model(lstm_model, "model.json")
+lstm_model = load_model("model.json")
+evaluate(lstm_model)
